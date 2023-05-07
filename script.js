68d387a4c049e3b16f45ea37a73663372c429a83
Vitor Souza <vitor.souza@absoftware.com.br>
2023-05-07 17:59:34 -0300

    melhorando consumo de memoria ao criar variavel

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+var numero;
+
 function aumentar(){
-    var numero = parseInt(document.getElementById('numero').textContent);
+    numero = parseInt(document.getElementById('numero').textContent);
     document.getElementById('numero').textContent = numero + 1;
 }
 
 function diminuir(){
-    var numero = parseInt(document.getElementById('numero').textContent);
+    numero = parseInt(document.getElementById('numero').textContent);
     document.getElementById('numero').textContent = numero - 1;
 }
 
